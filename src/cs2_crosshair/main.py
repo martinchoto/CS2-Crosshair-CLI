3ca601ad95ce64dcb4e43f4bc8b22a61a7eb9ee4
martinchoto <martin_stalev03@abv.bg>
2026-02-27 20:54:02 +0200

    added find and list method finally will work later on the implementation, added new methods to util.py

--- src/cs2_crosshair/main.py
+++ src/cs2_crosshair/main.py
@@ -3,10 +3,13 @@ import readchar
 from rich.console import Console
 from rich.table import Table
 from rich.live import Live
+from rich.panel import Panel
+from rich.text import Text
 from typing import Optional
 
+
 from .models import Crosshair
-from .utils import validate_crosshair_code, validate_name, name_exists
+from .utils import validate_crosshair_code, validate_name, name_exists, find_by_name, find_by_id
 
 from .database import db 
 
@@ -19,6 +22,13 @@ def add(name: str, code: str):
     
     valid_code = validate_crosshair_code(code)
     
+    #Checks if the name is correct
+    
+    if not validate_name(name):
+        console.print("[bold red]Invalid name[/bold red]")
+        console.print("[yellow]Lenght of the name must be lower than 16 letters and higher than 1[/yellow]")
+        raise typer.Exit()
+    
     # Check if the code is the correct for our crosshair
     
     if not valid_code:
@@ -26,14 +36,6 @@ def add(name: str, code: str):
         console.print("[yellow]Must follow format: [bold]CSGO-XXXXX-XXXXX-XXXXX-XXXXX-XXXXX[/bold][/yellow]")
         raise typer.Exit()
 
-    #Checks if the name is correct
-    
-    if not validate_name(name):
-        console.print("[bold red]Invalid name[/bold red]")
-        console.print("[yellow]Lenght of the name must be lower than 16 letters and higher than 1[/yellow]")
-        raise typer.Exit()
-
-    
     if name_exists(db, name):
         console.print(f"[bold red]Error:[/bold red] A crosshair named '[yellow]{name}[/yellow]' already exists!")
         console.print("[dim]Use a different name or delete the old one first.[/dim]")
@@ -61,7 +63,9 @@ def list(
     )
 ):
     # Get request for all crosshairs
+    
     all_data = db.all()
+    
     if not show_all:
         records = [r for r in all_data if r.get("is_active", True)]
     else:
@@ -117,13 +121,42 @@ def list(
             break
 
 @app.command()
+def find(
+    name: Optional[str] = typer.Option(None, "--name", "-n", help="Search by crosshair name"),
+    id: Optional[int] = typer.Option(None, "--id", help="Search by crosshair ID")
+):
+    # Check for command arguments
+    
+    if name:
+        res = find_by_name(db, name)
+        item = res[0] if res else None
+    elif id:
+        item = find_by_id(db, id)
+    else:
+        console.print("[yellow]Please provide either --name (-n) or --id.[/yellow]")
+        raise typer.Exit()
+
+    # Handle no results
+    if not item:
+        console.print("[bold red]No crosshair found.[/bold red]")
+        return
+
+    # Display results
+    
+    status = "[bold green]ACTIVE[/bold green]" if item.get("is_active", True) else "[bold red]ARCHIVED[/bold red]"
+    
+    print(f"{item["name"]} - {item["code"]}")
+
+
+    
+    
+@app.command()
 def edit():
     
-    print("asdasd")
+    print("Editing...")
 @app.command()
 def delete():
-    print("deleting..")
-
+    print("Deleting..")
 if __name__ == "__main__":
     app()
     
--- src/cs2_crosshair/utils.py
+++ src/cs2_crosshair/utils.py
@@ -4,20 +4,32 @@ from tinydb import where
 # The standard CS2/CSGO share code pattern
 CS2_REGEX = r"^CSGO(-[a-zA-Z0-9]{5}){5}$"
 
+#Crosshair validation
+
 def validate_crosshair_code(code: str) -> str:
 
-    cleaned_code = code.upper().strip()
+    cleaned_code = code.strip()
     
     if re.match(CS2_REGEX, cleaned_code):
         return cleaned_code
     
     return None
+# Crosshair validation for name
 
 def validate_name(name: str) -> bool:
     
     return 1 < len(name) < 16
 
+# Checks the db if name exists already
+
 def name_exists(db, name: str) -> bool:
 
     result = db.search(where('name').map(lambda x: x.lower()) == name.lower())
     return len(result) > 0
+
+# finds the name of the crosshair
+def find_by_name(db, name: str):
+    return db.get(where('name').map(lambda x: x.lower()) == name.lower())
+# finds it by the id
+def find_by_id(db, doc_id: int):
+    return db.get(doc_id=doc_id)
