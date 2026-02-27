1537a764407878995a91e2db52f0d3ae4687fa66
martinchoto <martin_stalev03@abv.bg>
2026-02-27 18:34:35 +0200

    added listing for the crosshairs

--- src/cs2_crosshair/main.py
+++ src/cs2_crosshair/main.py
@@ -1,5 +1,10 @@
 import typer
+import readchar
 from rich.console import Console
+from rich.table import Table
+from rich.live import Live
+from typing import Optional
+
 from .models import Crosshair
 from .utils import validate_crosshair_code, validate_name, name_exists
 
@@ -15,12 +20,14 @@ def add(name: str, code: str):
     valid_code = validate_crosshair_code(code)
     
     # Check if the code is the correct for our crosshair
+    
     if not valid_code:
         console.print("[bold red]Invalid Code![/bold red]")
         console.print("[yellow]Must follow format: [bold]CSGO-XXXXX-XXXXX-XXXXX-XXXXX-XXXXX[/bold][/yellow]")
         raise typer.Exit()
 
     #Checks if the name is correct
+    
     if not validate_name(name):
         console.print("[bold red]Invalid name[/bold red]")
         console.print("[yellow]Lenght of the name must be lower than 16 letters and higher than 1[/yellow]")
@@ -39,13 +46,75 @@ def add(name: str, code: str):
 
     new_crosshair = Crosshair(name=name, code=valid_code)
     
+    #Adds the crosshair to our small "db"
+    
     doc_id = db.insert(new_crosshair.to_dict())
     
     console.print(f"[green]✔ Saved '[bold]{new_crosshair.name}[/bold]' (ID: {doc_id})[/green]")
     
 @app.command()
-def list():
-    print("Listing...")
+def list(
+    show_all: bool = typer.Option(False, "--all", help="Show all crosshairs even deleted ones."),
+    page_size: int = typer.Option(
+        10, "--page-size", "-p", 
+        help="Number of crosshairs to show per page."
+    )
+):
+    # Get request for all crosshairs
+    all_data = db.all()
+    if not show_all:
+        records = [r for r in all_data if r.get("is_active", True)]
+    else:
+        records = all_data
+
+    if not records:
+        console.print("[yellow]Vault is empty.[/yellow]")
+        return
+
+    total_pages = (len(records) - 1) // page_size + 1
+    current_page = 0
+
+    while True:
+        console.clear() # Clears the terminal
+        
+        
+        # Creates the pagination
+        start = current_page * page_size
+        end = start + page_size
+        chunk = records[start:end]
+
+        # Table creation for the crosshairs
+        
+        table = Table(
+            title=f"CS2 Vault [bold cyan](Page {current_page + 1}/{total_pages})[/bold cyan]",
+            caption="[dim]Use ← → Arrows to Navigate | 'q' to Exit[/dim]"
+        )
+        
+        
+        table.add_column("ID", style="sky_blue3")
+        table.add_column("Name", style="grey70")
+        table.add_column("Code", style="pale_green3")
+        table.add_column("Status", style="medium_purple3")
+        
+        for item in chunk:
+            status = "[green]Active[/green]" if item.get("is_active", True) else "[red]Archived[/red]"
+            table.add_row(str(item.doc_id), item["name"], item["code"], item["created_at"])
+
+        console.print(table)
+
+        # This handles the arrow keys 
+        key = readchar.readkey()
+
+        if key == readchar.key.RIGHT:
+            if current_page < total_pages - 1:
+                current_page += 1
+        elif key == readchar.key.LEFT:
+            if current_page > 0:
+                current_page -= 1
+        elif key.lower() == 'q':
+            console.clear()
+            console.print("[yellow]Exited Vault list.[/yellow]")
+            break
 
 @app.command()
 def edit():
